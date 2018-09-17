e1907a7a39f0ba2a11eeb863ad8498b93b4d439f
EC2 Default User <ec2-user@ip-172-31-0-13.us-west-2.compute.internal>
2018-09-17 22:36:47 +0000

    updating for k8s

--- src/server.cr
+++ src/server.cr
@@ -1,6 +1,5 @@
 require "logger"
 require "http/server"
-require "./default_ip"
 
 log = Logger.new(STDOUT)
 log.level = Logger::DEBUG
@@ -10,29 +9,15 @@ log.info("Program started")
 log.warn("Nothing to do!")
 
 code_hash = File.read("code_hash.txt")
+ip = ENV["IP"]
+az = ENV["AZ"]
 log.debug("code_hash is: #{code_hash}")
-
-default_ip = DefaultIf.getip
-log.debug("default_ip is: #{default_ip}")
-
-if default_ip
-  net_octet = default_ip.split(".")[2]
-  log.debug("net_octet is: #{net_octet}")
-end
-
-case net_octet
-  when "100"
-  	az = "1a"
-  when "101"
-  	az = "1b"
-  when "102"
-  	az = "1c"
-else
-	az = "unknown"
-end
+log.debug("ip is: #{ip}")
 log.debug("az is: #{az}")
 
-server = HTTP::Server.new("0.0.0.0", 3000,
+az_message = ENV["AZ"]
+
+server = HTTP::Server.new(
   [
     HTTP::ErrorHandler.new,
     HTTP::LogHandler.new,
@@ -40,7 +25,7 @@ server = HTTP::Server.new("0.0.0.0", 3000,
     ]) do |context|
       if context.request.path == "/crystal" || context.request.path == "/crystal/"
         context.response.content_type = "text/plain"
-        context.response.print "Crystal backend: Hello! from #{default_ip} in AZ-#{az} commit #{code_hash}"
+        context.response.print "Crystal backend: Hello! from #{az_message} commit #{code_hash}"
       elsif context.request.path == "/health"
         context.response.content_type = "text/plain"
         context.response.print "Healthy!"
@@ -52,4 +37,5 @@ server = HTTP::Server.new("0.0.0.0", 3000,
     end
 
 puts "Listening on http://0.0.0.0:3000"
+server.bind_tcp "0.0.0.0", 3000
 server.listen
